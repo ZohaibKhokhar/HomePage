2bbabf2d8ce2e47b85fc00f6b56d0de8172c629b
bsef21m039@pucit.edu.pk <bsef21m039@pucit.edu.pk>
2024-11-20 16:32:09 +0500

    home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+const Home = () => {
+    return ( 
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
+     );
+}
+ 
+export default Home;
+<div>
+
+</div>
